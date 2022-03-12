ac58d60f961c5db5ea97c8ff909d910b8c966e90
Khaled21Ziedan <93595924+Khaled21Ziedan@users.noreply.github.com>
2022-03-12 17:10:33 +0300

    first

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: OpenningPage(),
       ),
     );
+
+class OpenningPage extends StatelessWidget {
+  const OpenningPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("images/OpeningPage.jpg"), fit: BoxFit.cover),
+      ),
+    );
+  }
+}
+
+
+class LibraryFirstPage extends StatefulWidget {
+  const LibraryFirstPage({Key key}) : super(key: key);
+
+  @override
+  _LibraryFirstPageState createState() => _LibraryFirstPageState();
+}
+
+class _LibraryFirstPageState extends State<LibraryFirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
